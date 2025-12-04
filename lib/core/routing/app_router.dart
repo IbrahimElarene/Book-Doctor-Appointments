8c24e72c27923600a70595f5bf37129f64db23c3
IbrahimElarene <elareneibrahim@gmail.com>
2025-12-04 20:33:53 +0200

    add sign up  screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -7,6 +7,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../features/login/logic/login_cubit.dart';
 import '../../features/onboarding/ui/onboarding_screen.dart';
+import '../../features/sign_up/logic/sign_up_cubit.dart';
+import '../../features/sign_up/ui/sign_up_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -18,6 +20,13 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => BlocProvider(
 
             create: (context) => getIt<LoginCubit>(), child: LoginScreen()));
+      case Routes.signUpScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<SignupCubit>(),
+            child: const SignupScreen(),
+          ),
+        );
       case Routes.homeScreen:
         return MaterialPageRoute(builder: (_) => const HomeScreen());
       default:
